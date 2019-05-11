3dc79dfe3e16219f53c26c17bc0300ae81220a0d
thomasfrl <feral.thomas@gmail.com>
2019-05-12 00:55:15 +0200

    init method and attribut  for model

--- model/comment.rb
+++ model/comment.rb
@@ -1,3 +1,19 @@
 class Comment
+  attr_accessor :content, :post_id
 
+  def initialize(post_id, content)
+
+  end
+  
+  def self.find(id)
+
+  end
+
+  def self.all
+
+  end
+
+  def self.select(post_id)
+
+  end
 end
--- model/post.rb
+++ model/post.rb
@@ -1,3 +1,20 @@
 class Post
+  attr_accessor :title, :content, :rating, :photo
+
+  def initialize(title, content, rating, photo)
+
+  end
+  
+  def self.find(id)
+
+  end
+
+  def self.all
+
+  end
+
+  def self.select(post_id)
+
+  end
 
 end

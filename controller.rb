71661416edcb7bcd604293f9c1b6ea093055c953
thomasfrl <feral.thomas@gmail.com>
2019-05-12 00:40:41 +0200

    init routes

--- controller.rb
+++ controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < Sinatra::Base
 	get '/' do
-    erb :index, :layout => :layout
+    erb :index, :layout => :layout, locals: {posts: Post.all}
+  end
+  get '/post/:id' do |id|
+    erb :show, :layout => :layout, locals: {post: Post.all[id.to_i], comments: Comment.select(id) }
   end
 end
